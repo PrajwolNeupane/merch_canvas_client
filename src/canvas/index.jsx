4838fcbe926441d1074b0e6d80935ed79c77442e
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-08-20 19:15:53 +0545

    Working on Three.js -added folders

--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { Environment, Center } from "@react-three/drei";
 
-function Canvas() {
-  return <div>Canvas</div>;
+import Shirt from "./Shirt";
+import Backdrop from "./Backdrop";
+import CameraRig from "./CameraRig";
+
+function CanvasModal() {
+  return (
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <Environment preset="city" />
+      <CameraRig>
+        <Backdrop />
+        <Center>
+          <Shirt />
+        </Center>
+      </CameraRig>
+    </Canvas>
+  );
 }
 
-export default Canvas;
+export default CanvasModal;
