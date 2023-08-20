73692d2c3847741fbea76b24b4c0e963c402baa5
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-08-20 23:32:20 +0545

    Working on Tab Actions

--- src/canvas/CameraRig.jsx
+++ src/canvas/CameraRig.jsx
@@ -1,9 +1,9 @@
-import React, { useRef } from 'react';
-import { useFrame } from '@react-three/fiber';
-import { easing } from 'maath';
-import { useSnapshot } from 'valtio';
+import React, { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import { easing } from "maath";
+import { useSnapshot } from "valtio";
 
-import state from '../store';
+import state from "../store";
 
 const CameraRig = ({ children }) => {
   const group = useRef();
@@ -15,28 +15,27 @@ const CameraRig = ({ children }) => {
 
     // set the initial position of the model
     let targetPosition = [-0.4, 0, 2];
-    if(snap.intro) {
-      if(isBreakpoint) targetPosition = [0, 0, 2];
-      if(isMobile) targetPosition = [0, 0.2, 2.5];
+    if (snap.intro) {
+      if (isBreakpoint) targetPosition = [0, 0, 2];
+      if (isMobile) targetPosition = [0, 0.2, 2.5];
     } else {
-      if(isMobile) targetPosition = [0, 0, 2.5]
+      if (isMobile) targetPosition = [0, 0, 2.5];
       else targetPosition = [0, 0, 2];
     }
 
     // set model camera position
-    easing.damp3(state.camera.position, targetPosition, 0.25, delta)
+    easing.damp3(state.camera.position, targetPosition, 0.25, delta);
 
     // set the model rotation smoothly
     easing.dampE(
       group.current.rotation,
-      [-state.pointer.y / 2,state.pointer.x / 0.8, 0],
+      [-state.pointer.y / 2.5, state.pointer.x / 1, 0],
       0.25,
       delta
-    )
-  })
+    );
+  });
 
+  return <group ref={group}>{children}</group>;
+};
 
-  return <group ref={group}>{children}</group>
-}
-
-export default CameraRig
+export default CameraRig;
