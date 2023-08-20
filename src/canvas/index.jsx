c866810d8755ba7f0701689a4933c3dc266ff91c
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-08-20 23:09:37 +0545

    Added 3D Modal

--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -1,16 +1,21 @@
-import React from "react";
-import { Canvas } from "@react-three/fiber";
-import { Environment, Center } from "@react-three/drei";
+import { Canvas } from '@react-three/fiber'
+import { Environment, Center } from '@react-three/drei';
 
-import Shirt from "./Shirt";
-import Backdrop from "./Backdrop";
-import CameraRig from "./CameraRig";
+import Shirt from './Shirt';
+import Backdrop from './Backdrop';
+import CameraRig from './CameraRig';
 
-function CanvasModal() {
+const CanvasModel = () => {
   return (
-    <Canvas>
+    <Canvas
+      shadows
+      camera={{ position: [0, 0, 0], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+      className="w-full max-w-full h-full transition-all ease-in"
+    >
       <ambientLight intensity={0.5} />
       <Environment preset="city" />
+
       <CameraRig>
         <Backdrop />
         <Center>
@@ -18,7 +23,7 @@ function CanvasModal() {
         </Center>
       </CameraRig>
     </Canvas>
-  );
+  )
 }
 
-export default CanvasModal;
+export default CanvasModel
