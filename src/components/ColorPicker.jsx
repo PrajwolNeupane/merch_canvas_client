d12d57061c66b5317455fa7b5481944be8d69147
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-08-21 11:11:43 +0545

    Added Color Picker Features

--- src/components/ColorPicker.jsx
+++ src/components/ColorPicker.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import { SketchPicker } from "react-color";
+import { useSnapshot } from "valtio";
+import state from "../store";
 
 function ColorPicker() {
+  const snap = useSnapshot(state);
+
   return (
-    <div>
-      ColorPicker
+    <div className="absolute left-full ml-3">
+      <SketchPicker
+        color={snap.color}
+        disableAlpha
+        onChange={(color) => {
+          state.color = color.hex;
+        }}
+      />
     </div>
-  )
+  );
 }
 
-export default ColorPicker
+export default ColorPicker;
